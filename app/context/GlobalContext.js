3c456eb527ef2c6d763c95f63f9873c8f26eebd8
Annie McMahon <108587699+AnnieMcMahon@users.noreply.github.com>
2024-11-21 11:33:32 -0500

    fixed minor styling issue

--- app/context/GlobalContext.js
+++ app/context/GlobalContext.js
@@ -2,13 +2,11 @@
 import { createContext, useState, useContext, useEffect } from "react";
 import { retrieveUser } from "@/app/api/authentication/auth";
 import { fetchUserByEmail } from "@/app/api/data/data";
-
 import { defaultUser } from "@/app/lib/defaultContent";
 
 const GlobalContext = createContext();
 
 export function GlobalProvider({ children }) {
-  //Initialize states with sample data
   const [currentUser, setCurrentUser] = useState(defaultUser);
   const [hike, setHike] = useState();
   
