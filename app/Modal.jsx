04c96ed8948cf07f97e27efdda39124cf3c7dc2f
Annie McMahon <108587699+AnnieMcMahon@users.noreply.github.com>
2025-03-03 15:26:46 -0500

    fixed modal styling (margin, padding, buttons)

--- app/Modal.jsx
+++ app/Modal.jsx
@@ -8,13 +8,13 @@ export default function Modal() {
 
   return (
     <div className="fixed inset-0 bg-black bg-opacity-50 flex justify-center items-center z-[1000]">
-      <div className="bg-gray-100 p-8 w-80 text-center border border-green-500 rounded-md">
-        <h2>{modal.title}</h2>
-        <p className="text-sm">{modal.message}</p>
+      <div className="bg-gray-100 p-2 w-80 text-center border border-green-500 rounded-md">
+        <h2 className="p-4">{modal.title}</h2>
+        <p className="text-sm mx-4 mt-2 mb-4 text-left">{modal.message}</p>
         {modal.onConfirm && (
-          <button className="w-24 mt-4" onClick={modal.onConfirm}>Confirm</button>
+          <button className="w-24 mx-2 text-sm" onClick={modal.onConfirm}>Confirm</button>
         )}
-        <button className="w-24 mt-4 text-sm" onClick={closeModal}>Close</button>
+        <button className="w-24 mx-2 text-sm" onClick={closeModal}>Close</button>
       </div>
     </div>
   );
