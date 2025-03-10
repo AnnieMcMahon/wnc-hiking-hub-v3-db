e7625b0cf8fe0e1866493f36f37cf1d81e736c37
Annie McMahon <108587699+AnnieMcMahon@users.noreply.github.com>
2025-03-10 15:24:56 -0400

    a few more styling tweaks

--- app/ui/forms/LoginForm.jsx
+++ app/ui/forms/LoginForm.jsx
@@ -46,7 +46,7 @@ export default function LoginForm({ onSubmit = () => {}, onClick = () => {}  }) 
         required
       />
       <br/>
-      <a href="#" className="ml-2 text-green-800 hover:text-green-600 text-sm" onClick={handleClick}>Forgot password?</a>
+      <a href="#" onClick={handleClick}>Forgot password?</a>
       <br />
       <p className="my-4">Password must be at least six characters and must contain at least one uppercase letter, one number, and one special character</p>
       <button type="submit" className="form-button">
