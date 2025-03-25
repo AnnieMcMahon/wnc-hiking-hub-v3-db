8c556a07f500d24fdb4c9058b5717c375020cb81
Annie McMahon <108587699+AnnieMcMahon@users.noreply.github.com>
2025-03-25 15:58:17 -0400

    disabled link to participant bio in demo mode

--- app/ui/components/Participant.jsx
+++ app/ui/components/Participant.jsx
@@ -2,37 +2,57 @@
 import { Avatar, AvatarFallback, AvatarImage } from "@/components/ui/avatar";
 import { fetchUserById } from "@/app/api/data/data";
 import { useState, useEffect } from "react";
+import { useGlobal } from "@/app/context/GlobalContext";
 import Link from "next/link";
 
-export default function Participant({participant}) {
+export default function Participant({ participant }) {
   const [participantData, setParticipantData] = useState({});
+  const { currentUser } = useGlobal();
 
   useEffect(() => {
     const fetchData = async () => {
       const userInfo = await fetchUserById(participant.user_id);
-      const user_name = userInfo[0]?.user_name ? userInfo[0].user_name : "unknown";
-      const avatar = userInfo[0]?.avatar ? userInfo[0].avatar : "newUser.png"
+      const user_name = userInfo[0]?.user_name
+        ? userInfo[0].user_name
+        : "unknown";
+      const avatar = userInfo[0]?.avatar ? userInfo[0].avatar : "newUser.png";
       setParticipantData({
         user_name: user_name,
         avatar: avatar,
-      })
-    }
+      });
+    };
     fetchData();
   }, [participant.user_id]);
 
   return (
-    <Link href={`/bio/${participant.user_id}`} passHref>
-      <div className="container border border-gray-400 p-2 cursor-pointer">
-        <div className="flex flex-row gap-4">
-          <Avatar className="w-4 h-4">
-            <AvatarImage src={participantData.avatar} />
-            <AvatarFallback>Avatar</AvatarFallback>
-          </Avatar>
-          <div className="flex justify-between w-full text-xs">
-            <div>{participantData.user_name}</div>
+    <>
+      {currentUser?.id !== 1 ? (
+        <Link href={`/bio/${participant.user_id}`} passHref>
+          <div className="container border border-gray-400 p-2 cursor-pointer">
+            <div className="flex flex-row gap-4">
+              <Avatar className="w-4 h-4">
+                <AvatarImage src={participantData.avatar} />
+                <AvatarFallback>Avatar</AvatarFallback>
+              </Avatar>
+              <div className="flex justify-between w-full text-xs">
+                <div>{participantData.user_name}</div>
+              </div>
+            </div>
+          </div>
+        </Link>
+      ) : (
+        <div className="container border border-gray-400 p-2">
+          <div className="flex flex-row gap-4">
+            <Avatar className="w-4 h-4">
+              <AvatarImage src={participantData.avatar} />
+              <AvatarFallback>Avatar</AvatarFallback>
+            </Avatar>
+            <div className="flex justify-between w-full text-xs">
+              <div>{participantData.user_name}</div>
+            </div>
           </div>
         </div>
-      </div>
-    </Link>
+      )}
+    </>
   );
 }
