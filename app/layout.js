a9ca63eb3bddcab8e9a9d0577789ca760fb496c4
Annie McMahon <108587699+AnnieMcMahon@users.noreply.github.com>
2025-04-06 10:53:34 -0400

    added Google Analytics tracking

--- app/layout.js
+++ app/layout.js
@@ -1,5 +1,6 @@
 import { GlobalProvider } from "@/app/context/GlobalContext";
 import { ModalProvider } from "@/app/context/ModalContext";
+import Script from "next/script";
 import Modal from "./Modal";
 import Navbar from "./ui/components/Navbar";
 import "./global.css";
@@ -17,6 +18,19 @@ export default function RootLayout({ children }) {
     <html lang="en">
       <body className="h-[100vh] text-center bg-no-repeat bg-center bg-[url('/background.jpeg')] bg-cover"
         >   
+        {/* Google Analytics Script */}
+        <Script
+          async
+          src="https://www.googletagmanager.com/gtag/js?id=G-L5Q33L0NSS"
+        />
+        <Script id="google-analytics">
+          {`
+            window.dataLayer = window.dataLayer || [];
+            function gtag(){dataLayer.push(arguments);}
+            gtag('js', new Date());
+            gtag('config', 'G-L5Q33L0NSS');
+          `}
+        </Script>
         <GlobalProvider>
           <ModalProvider>
             <Modal />
